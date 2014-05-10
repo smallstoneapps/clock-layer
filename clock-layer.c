3a4e84631027a42e94a1ae5fb38461903dfc92b1
Matthew Tole <matthewtole@gmail.com>
2014-05-10 16:44:58 +0100

    Fix memory issue and update to v1.1

--- clock-layer.c
+++ clock-layer.c
@@ -1,7 +1,38 @@
-/***
- * Clock Layer
- * Copyright © 2013 Matthew Tole
- ***/
+/*
+
+Clock Layer v1.1
+A Pebble library for displaying a formatted time / date
+http://smallstoneapps.github.io/clock-layer/
+
+----------------------
+
+The MIT License (MIT)
+
+Copyright © 2013 - 2014 Matthew Tole
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+--------------------
+
+clock-layer.c
+
+*/
 
 #include <pebble.h>
 #include "clock-layer.h"
@@ -18,7 +49,8 @@ ClockLayer* clock_layer_create(GRect frame) {
   text_layer_set_text_alignment(clock_layer->text_layer, GTextAlignmentCenter);
   text_layer_set_text_color(clock_layer->text_layer, GColorBlack);
   text_layer_set_background_color(clock_layer->text_layer, GColorClear);
-  clock_layer->format = "%H:%M";
+  clock_layer->format = NULL;
+  clock_layer_set_time_format(clock_layer, "%H:%M");
   return clock_layer;
 }
 
